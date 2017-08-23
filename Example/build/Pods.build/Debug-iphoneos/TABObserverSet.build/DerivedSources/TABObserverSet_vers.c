dd52869065c929ab18a9e5c652793b2069728beb
Luqman Fauzi <luckman.fauzi@gmail.com>
2017-08-23 10:40:48 +0800

    Bump deployment target to iOS 10

---
+++ Example/build/Pods.build/Debug-iphoneos/TABObserverSet.build/DerivedSources/TABObserverSet_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char TABObserverSetVersionString[];
+ extern const double TABObserverSetVersionNumber;
+
+ const unsigned char TABObserverSetVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:TABObserverSet  PROJECT:Pods-1" "\n";
+ const double TABObserverSetVersionNumber __attribute__ ((used)) = (double)1.;
